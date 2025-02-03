678bab12ac3309e462037f0a6e97072fef3e504e
Mario Alves <marioalvesneto@hotmail.com>
2025-02-02 21:45:24 -0300

    feat: Config Firebase integration

---
+++ src/services/firebase/useProjects.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebaseConfig";
+
+export function useProjects() {
+  const [projects, setProjects] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "projects"));
+      console.log(
+        querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      );
+
+      setProjects(
+        querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      );
+    };
+
+    fetchData();
+  }, []);
+
+  return { projects };
+}
